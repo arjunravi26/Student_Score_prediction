07ae50887ed48f77a7de0ed9e57989668d75f472
Arjun Ravi <arjunravi1523@gmail.com>
2024-04-24 20:04:51 +0530

    Data Transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -80,8 +80,8 @@ class DataTransformation:
             input_feature_test_arr = preprocessing_obj.transform(
                 input_feature_test_df
             )
-            train_arr = np.c_[input_feature_train_arr, np.array(input_feature_train_df)]
-            test_arr = np.c_[input_feature_test_arr, np.array(input_feature_test_df)]
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
             logging.info("Saved preprocessing object")
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
